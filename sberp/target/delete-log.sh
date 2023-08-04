55621ebd467879bb935bd1bf8666e4a3a86590e6
welovex2 <127914340+welovex2@users.noreply.github.com>
2023-08-04 17:53:56 +0900

    Delete sberp/target directory

--- sberp/target/delete-log.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "로그파일 삭제 시작"
-
-find /stand500admin/tomcat/logs/ -mtime +30 -name catalina\* -exec rm {} \; 
-find /stand500admin/tomcat/logs/ -mtime +30 -name \* -exec rm {} \;
-
-echo "로그파일 삭제 완료"
