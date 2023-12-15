3860e7d3975eb60283f74e488e2a7453d472309d
welovex2 <127914340+welovex2@users.noreply.github.com>
2023-12-15 10:22:35 +0900

    프로젝트상태 일괄변경기능 (#21)

--- sberp/src/main/java/egovframework/cmm/service/ResponseMessage.java
+++ sberp/src/main/java/egovframework/cmm/service/ResponseMessage.java
@@ -40,4 +40,5 @@ public class ResponseMessage {
     public static final String GO_CHQ = "취합견적서에서 진행하세요";
     
     public static final String MAX_REVISION = "이정도 했으면 신청서를 새로 받읍시다";
+    public static final String CHECK_JOB_SEQ = "상태변경할 문서가 없습니다.";
 }
--- sberp/src/main/java/egovframework/cmm/web/JobController.java
+++ sberp/src/main/java/egovframework/cmm/web/JobController.java
@@ -1,6 +1,7 @@
 package egovframework.cmm.web;
 
 import javax.annotation.Resource;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 import egovframework.cmm.service.BasicResponse;
 import egovframework.cmm.service.CmmService;
 import egovframework.cmm.service.Job;
+import egovframework.cmm.service.JobVO;
 import egovframework.cmm.service.LoginVO;
 import egovframework.cmm.service.ResponseMessage;
 import egovframework.cmm.util.EgovUserDetailsHelper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -51,6 +54,51 @@ public class JobController {
               .build();
       
       return res;
+  }
+  
+
+  @ApiOperation(value = "프로젝트상태 일괄변경")
+  @PostMapping(value = "/all/testStateInsert.do")
+  public BasicResponse testStateAllInert(@ApiParam(value = "jobSeqs[], stateCode 전송") @RequestBody JobVO req)
+      throws Exception {
+
+    LoginVO user = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
+    boolean result = true;
+    String msg = "";
+
+    // 로그인정보
+    req.setInsMemId(user.getId());
+
+    System.out.println("================");
+    System.out.println(req);
+    System.out.println("================");
+    Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+
+    int success = 0;
+    Job item = new Job();
+    if (isAuthenticated) {
+
+      // 2개 이상부터 취합 가능
+      if (ObjectUtils.isEmpty(req.getJobSeqs())) {
+        result = false;
+        msg = ResponseMessage.CHECK_JOB_SEQ;
+      }
+      else {
+        item.setStateCode(req.getStateCode());
+        for (int d : req.getJobSeqs()) {
+          
+          item.setJobSeq(d);
+          if (cmmService.jobStateUpdate(item)) success++;
+          
+        }
+  
+        msg = Integer.toString(success).concat("건 처리완료되었습니다.");
+      }
+    }
+
+    BasicResponse res = BasicResponse.builder().result(result).message(msg).build();
+
+    return res;
   }
   
 }
